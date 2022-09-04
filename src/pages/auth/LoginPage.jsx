46865b49401bba0b74f96f70ffd2d0314bec139f
karvaroz <vargasriosk@gmail.com>
2022-09-03 19:16:56 -0500

    style login and register

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+
+import LoginImg from "../../assets/login.svg";
+
+import { Link } from "react-router-dom"
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+	return (
+		<div className="content">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6 order-md-2">
+						<img
+							src={LoginImg}
+							alt="Image"
+							className="img-fluid"
+						/>
+					</div>
+					<div className="col-md-6 contents">
+						<div className="row justify-content-center">
+							<div className="col-md-8">
+								<div className="mb-4">
+									<h3>
+										<strong>Login</strong>
+									</h3>
+									<p className="mb-4">Ingresa con tu correo y contraseña</p>
+								</div>
+								<form
+									action="#"
+									method="post">
+									<div className="form-group first">
+										<label for="username">Correo</label>
+										<input
+											type="text"
+											className="form-control"
+											id="username"
+										/>
+									</div>
+									<div className="form-group last mb-4">
+										<label for="password">Contraseña</label>
+										<input
+											type="password"
+											className="form-control"
+											id="password"
+										/>
+									</div>
+									<input
+										type="submit"
+										value="Ingresar"
+										className="btn text-white btn-block btn-primary"
+									/>
+									<Link to="/register">
+										<span className="d-block text-left my-4 text-muted">
+											¿No tienes cuenta? Registrate
+										</span>
+									</Link>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default LoginPage
+export default LoginPage;
--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import RegisterImg from "../../assets/register.svg";
+import { Link } from "react-router-dom"
 
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+	return (
+		<div class="content">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<img
+							src={RegisterImg}
+							alt="Image"
+							class="img-fluid"
+						/>
+					</div>
+					<div class="col-md-6 contents">
+						<div class="row justify-content-center">
+							<div class="col-md-8">
+								<div class="mb-4">
+									<h3>Registrarte</h3>
+									<p class="mb-4">
+										Crea una cuenta, ingresando los siguientes datos
+									</p>
+								</div>
+								<form
+									action="#"
+									method="post">
+									<div class="form-group first">
+										<label for="username">Usuario</label>
+										<input
+											type="text"
+											class="form-control"
+											id="username"
+										/>
+									</div>
+									<div class="form-group last mb-4">
+										<label for="password">Contraseña</label>
+										<input
+											type="password"
+											class="form-control"
+											id="password"
+										/>
+									</div>
 
-export default RegisterPage
+									<input
+										type="submit"
+										value="Log In"
+										class="btn btn-block btn-primary"
+									/>
+									<Link to="/login">
+										<span class="d-block text-left my-4 text-muted">
+											&mdash; ¿Ya tienes una cuenta? Ingresa &mdash;
+										</span>
+									</Link>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default RegisterPage;
