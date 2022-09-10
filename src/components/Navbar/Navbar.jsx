be994d6a8f3961afcb936b33e84b7d27ccdc28b5
karvaroz <vargasriosk@gmail.com>
2022-09-10 17:33:58 -0500

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,17 @@ const Navbar = () => {
 	return (
 		<header
 			id="header"
-			LclassName="header fixed-top"></header>
+			className="fixed-top navbar navbar bg-dark mb-4 px-4">
+			<span className="navbar-brand text-white">
+				<ion-icon name="calendar-outline"></ion-icon>
+				&nbsp; Karina
+			</span>
+			<button>
+				<ion-icon name="log-out-outline"></ion-icon>
+				&nbsp;
+				<span>Salir</span>
+			</button>
+		</header>
 	);
 };
 
--- src/pages/agenda/AgendaPage.jsx
+++ src/pages/agenda/AgendaPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from "../../components/Navbar/Navbar";
+
 
 const AgendaPage = () => {
   return (
-    <div>AgendaPage</div>
+    <Navbar />
   )
 }
 
