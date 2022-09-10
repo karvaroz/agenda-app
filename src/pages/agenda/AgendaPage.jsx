b805f48c17ce4c718a8418465a311b0a6e5a1d80
karvaroz <vargasriosk@gmail.com>
2022-09-10 17:49:16 -0500

    add calendar

--- src/pages/agenda/AgendaPage.jsx
+++ src/pages/agenda/AgendaPage.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
 import Navbar from "../../components/Navbar/Navbar";
 
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import enUS from "date-fns/locale/en-US";
+
+import { addHours, parse, startOfWeek, getDay, format } from "date-fns";
+
+const locales = {
+	"en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+	format,
+	parse,
+	startOfWeek,
+	getDay,
+	locales,
+});
+
+const events = [
+	{
+		title: "Esto es un evento de prueba",
+		notes: "Para ver si funciona",
+		start: new Date(),
+		end: addHours(new Date(), 2),
+		bgColor: "rgba(255, 255, 255, 0.5)",
+		user: {
+			_id: "001",
+			name: "Karina",
+		},
+	},
+];
 
 const AgendaPage = () => {
-  return (
-    <Navbar />
-  )
-}
+	return (
+		<>
+			<Navbar />
 
-export default AgendaPage
+			<Calendar
+				className="p-5"
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+				style={{ height: "calc( 100vh - 200px)", width: "100vw" }}
+			/>
+		</>
+	);
+};
+
+export default AgendaPage;
